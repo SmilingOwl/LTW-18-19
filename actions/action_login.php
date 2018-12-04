cbfd81abe4f9bcb573d80881c7b3ed7d8b4fbb7c
SmilingOwl <julianamcm05@gmail.com>
2018-12-04 19:57:37 +0000

    Add files via upload

--- actions/action_login.php
+++ actions/action_login.php
@@ -2,15 +2,14 @@
   include_once('../includes/session.php');
   include_once('../database/database_user.php');
   
-
+  $username=$_POST['username'];
+  $password=$_POST['password'];
   
-  if(($userID = VerifyLogin($_POST['username'], $_POST['password'])) != -1){
-	setCurrentUser($userID, $_POST['username']);
-	header("Location:../php/homepage.php");
-} 
+  if($userID = verifyLogin($username, $password) != -1) {
+    setCurrentUser($userID, $username);
+    header("Location:../php/homepage.php");}
 
-else {
-	header('Location: ../php/first_page.php');
-}
-
+  else {
+    header("Location:../php/first_page.php");
+  }
 ?>
