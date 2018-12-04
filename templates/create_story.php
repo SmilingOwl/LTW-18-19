6c6a6fbd7215acd69ff8b2c094e6a9e2107f8726
helenaMontenegro <36671963+helenaMontenegro@users.noreply.github.com>
2018-12-04 10:06:18 +0000

    Add files via upload

--- templates/create_story.php
+++ templates/create_story.php
@@ -1,16 +1,8 @@
-<?php $taste_choices=get_taste_choices(); ?>
-
-<section id=create_story>
-    <form>
-        <label>Title: <textarea name="title"></textarea></label>
-        <textarea name="text"> Write your story here! </textarea>
-        <textarea name="image"> Write the name of the image here! </textarea>
-        <input type="hidden" name="user_id" value="<?=$user_id?>">
-        <select name="tasteChoice">
-            <?php foreach($taste_choices as $taste) {?>
-                <option value="<?=$taste['id_taste']?>"><?=$taste['taste']?></option>
-            <?php } ?>
-        </select>
-        <input type="submit" value="Submit">
-    </form>
-</section>
+<?php 
+    include_once('../database/connection.php');
+    include_once('../database/access_database.php');
+    
+    $taste_choices = get_taste_choices();
+    
+    echo json_encode( $taste_choices);
+?>
