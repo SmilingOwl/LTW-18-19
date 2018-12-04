64c5e85d1dfa527184d4323b1b4100170f11a1b5
SmilingOwl <julianamcm05@gmail.com>
2018-12-04 19:57:03 +0000

    Add files via upload

--- database/database_user.php
+++ database/database_user.php
@@ -7,4 +7,22 @@
     $stmt->execute(array($username, $email, $birthdate, $photo, sha1($password), $gender));
   }
 
+  function verifyLogin($username, $password) {
+    $db = Database::instance()->db();
+    $stmt = $db->prepare('SELECT * FROM Users WHERE username = ? AND password = ?');
+    $stmt->execute(array($username, $password));
+    if($stmt->fetch() !== false)
+      return getID($username);
+    else return -1;
+  }
+
+  function getID($username) {
+    $db = Database::instance()->db();
+    $stmt = $db->prepare('SELECT user_id FROM Users WHERE username = ?');
+    $stmt->execute(array($username));
+    $var=$stmt->fetch();
+    if($var !== false)
+      return $var['user_id'];
+    return -1;
+  }
  ?>
