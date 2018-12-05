bdeab564aa4e12704ef8f346c2a1440123636b5a
helenaMontenegro <36671963+helenaMontenegro@users.noreply.github.com>
2018-12-05 19:10:29 +0000

    Add files via upload

--- scripts/show_comments.js
+++ scripts/show_comments.js
@@ -1,39 +1,49 @@
-/*comments = document.querySelectorAll('#stories span.comments');
+comments = document.querySelectorAll('#stories span.comments');
+let footer;
+showing = [];
 
-let show_comments = function() {
+function encodeForAjax(data) {
+    return Object.keys(data).map(function(k){
+        return encodeURIComponent(k) + '=' + encodeURIComponent(data[k])
+    }).join('&')
+}
+
+let show_comments = function()  {
     footer = this.parentNode;
-    story = footer.parentNode;
+    if (this.showing) {
+        let comments_section = footer.parentNode.querySelector('#comments_section');
+        footer.parentNode.removeChild(comments_section);
+        this.showing = false;
+        return;
+    }
+    this.showing = true;
+    let story_id = footer.querySelector('input').value;
+    console.log(story_id);
+
+    let request = new XMLHttpRequest();
+    request.addEventListener('load', receive_answer_show_comments);
+    request.open('POST', '../actions/get_comments.php', true);
+    request.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+    request.send(encodeForAjax({story_id: story_id}));
+}
+
+let receive_answer_show_comments = function() {
+    let story = footer.parentNode;
     console.log(story);
+    let comments_to_show = JSON.parse(this.responseText);
+    
+    console.log(comments_to_show);
     let new_comment_section = document.createElement('section');
     new_comment_section.id = "comments_section";
-    new_comment_section.innerHTML = ajax('../templates/show_comments.php');
-    // '<?php' +
-    //         '$comments = get_comments_in_story($story_id)' + 
-    //         'foreach ($comments as $comment) { ' +
-    //         '$comments_likes = get_likes_comment($comment[\'id_comment\']);' +
-    //         '$comments_dislikes = get_dislikes_comment($comment[\'id_comment\']);' +
-    //         '$likes_to_write=\'likes\';' +
-    //         'if(count($comments_likes) == 1)' +
-    //             '$likes_to_write=\'like\';' +
-
-    //         '$dislikes = get_dislikes_story($story[\'story_id\']);' +
-    //         '$dislikes_to_write=\'dislikes\';' +
-    //         'if(count($comments_dislikes) == 1)' +
-    //             '$dislikes_to_write=\'dislike\';' +
-    //     '?>' +
-    //         '<article class="comment">' +
-    //             '<?php $comment_user = get_user($comment[\'user_id\']);?>' +
-    //             '<span class="user"><?=$comment_user[\'username\']?> </span>' +
-    //             '<p><?=$comment[\'text\']?></p>' +
-    //             '<footer>' +
-    //                 '<span class="likes"><?=count($comments_likes)?> <?=$likes_to_write?></span>' +
-    //                 '<span class="dislikes"><?=count($comments_dislikes)?> <?=$dislikes_to_write?></span>' +
-    //             '</footer>' +
-    //         '</article>' +
-    //     '<?php } ?>';
+    for (let i = 0; i < comments_to_show.length; i++)
+    new_comment_section.innerHTML +=  '<article class="comment">' +
+        '<span class="user">' + comments_to_show[i].username + ' </span>' + 
+        '<p>' + comments_to_show[i].text + '</p>';
 
     story.appendChild(new_comment_section);
 }
 
-for(let i = 0; i < comments.length; i++)
-    comments[i].addEventListener('click', show_comments);*/
+for(let i = 0; i < comments.length; i++) {
+    comments[i].addEventListener('click', show_comments);
+    comments[i].showing=false;
+}
