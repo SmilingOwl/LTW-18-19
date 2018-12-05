15de50df3b80ad4021c6f4b2b3d4a099ff4d45cd
helenaMontenegro <36671963+helenaMontenegro@users.noreply.github.com>
2018-12-05 19:16:47 +0000

    Delete create_story.php

--- templates/create_story.php
+++
@@ -1,8 +0,0 @@
-<?php 
-    include_once('../database/connection.php');
-    include_once('../database/access_database.php');
-    
-    $taste_choices = get_taste_choices();
-    
-    echo json_encode( $taste_choices);
-?>
