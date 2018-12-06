5c597a39b416575dfe1ed19d84dcc5700370e9f0
helenaMontenegro <36671963+helenaMontenegro@users.noreply.github.com>
2018-12-06 17:57:57 +0000

    Add files via upload

--- scripts/add_favorite.js
+++ scripts/add_favorite.js
@@ -1,30 +1,18 @@
-let favorites_span = document.querySelector('#story span.favorites');
+let favorites_span = document.querySelectorAll('#story span.favorites');
+let selected_favorite;
 
 function encodeForAjax(data) {
     return Object.keys(data).map(function(k){
         return encodeURIComponent(k) + '=' + encodeURIComponent(data[k])
     }).join('&')
 }
- /* Part to color favorites */
-let color_favorite = function(event) {
-    console.log(this.responseText);
-    let answer = JSON.parse(this.responseText);
-    if(answer) {
-        favorites_span.style.color = "blue";
-    }
-}
-
-let request_favorites = new XMLHttpRequest();
-request_favorites.addEventListener('load', color_favorite);
-request_favorites.open('POST', '../actions/check_favorite.php', true);
-request_favorites.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
-request_favorites.send(encodeForAjax({story_id: story_id, user_id: user_id}));
 
 /* Part to add favorites on click */
 let add_favorite = function() {
-    let user_id=document.querySelector('input[name=user_id]').value;
-    let story_id=document.querySelector('input[name=story_id]').value;
-
+    let user_id=this.parentNode.querySelector('input[name=user_id]').value;    
+    let story_id=this.parentNode.querySelector('input[name=story_id]').value;
+    selected_favorite = this;
+    
     let request = new XMLHttpRequest();
     request.addEventListener('load', receive_favorite_count);
     request.open('POST', '../actions/add_favorite.php', true);
@@ -33,14 +21,36 @@ let add_favorite = function() {
 }
 
 let receive_favorite_count = function(event) {
+    
+console.log(selected_favorite);
     let answer = JSON.parse(this.responseText);
-
-    favorites_span.parentNode.removeChild(favorites_span);
+    let footer = selected_favorite.parentNode;
+    console.log(footer);
+    footer.removeChild(selected_favorite);
 
     let new_favorites = document.createElement('span');
     new_favorites.className = "favorites";
     new_favorites.innerHTML = answer + '<img src="../icons/saved_icon.png" alt="favorites"></img>';
     footer.appendChild(new_favorites);
+    new_favorites.style.color = "blue";
 }
 
-favorites_span.addEventListener('click', add_favorite);
+/* Part to color favorites */
+
+for(let i = 0; i < favorites_span.length; i++) {
+    favorites_span[i].addEventListener('click', add_favorite);
+
+    let user_id=favorites_span[i].parentNode.querySelector('input[name=user_id]').value;
+    let story_id=favorites_span[i].parentNode.querySelector('input[name=story_id]').value;
+
+    let request_favorites = new XMLHttpRequest();
+    request_favorites.addEventListener('load', function(){
+        let answer = JSON.parse(this.responseText);
+        if(answer) {
+            favorites_span[i].style.color = "blue";
+    }
+    });
+    request_favorites.open('POST', '../actions/check_favorite.php', true);
+    request_favorites.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+    request_favorites.send(encodeForAjax({story_id: story_id, user_id: user_id}));
+}
