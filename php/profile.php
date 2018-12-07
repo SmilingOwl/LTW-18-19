d62a72e1896f09f5cde452b36e3deeebd1d9c408
AnaDenisa <anasecuiu@yahoo.com>
2018-12-07 14:13:09 +0000

    Added css file, presentation and interests

--- php/profile.php
+++ php/profile.php
@@ -11,19 +11,39 @@
     include_once('../templates/common/header.php');
 ?>
 
-    <header>
-      <h1> Profile </h1>
-      <span class="menu"><img src="../icons/menu_icon.png" alt="Menu"></span>
-    </header>
+<header>
+  <title>User profile page</title>
+  <link href="../css/layout_profile.css" rel="stylesheet">
+  <span class="menu"><img src="../icons/menu_icon.png" alt="Menu"></span>
+</header>
 
-    <section id="presentation">
-        <h2><?=$user['username']?></h2>
-        <img src=<?=$user['photo']?> alt="Can't load picture">
+<body>
+    <img src=<?=$user['photo']?> alt="Can't load picture">
+    <h3><?=$user['username']?>'s Profile</h3>
+
+    <p id="presentation">
+        Lorem Ipsum is simply dummy text of the printing and typesetting
+        industry. Lorem Ipsum has been the industry's standard dummy text
+        ever since the 1500s, when an unknown printer took a galley of type
+        and scrambled it to make a type specimen book. It has survived not
+    </p>
+    <section id="interests">
+        <ul>
+            <li>Local</a></li>
+            <li>World</a></li>
+            <li>Politics</a></li>
+            <li>Sports</a></li>
+            <li>Science</a></li>
+            <li>Weather</a></li>
+        </ul>
     </section>
 
-    <h2> My Stories: </h2>
+    <section id="stories">
+        <h4> My Stories: </h4>
+        <?php include_once('../templates/show_stories.php');?>
+    </section>
+</body>
 
-    <?php 
-        include_once('../templates/show_stories.php');
-        include_once('../templates/common/footer.php');
-    ?>
+<footer>
+    <?php include_once('../templates/common/footer.php');?>
+</footer>
