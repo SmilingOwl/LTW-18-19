6f263580eeed6fff8cdfd79f5c23e7c4a28dad18
helenaMontenegro <36671963+helenaMontenegro@users.noreply.github.com>
2018-12-07 09:26:18 +0000

    Add files via upload

--- actions/action_login.php
+++ actions/action_login.php
@@ -1,11 +1,12 @@
 <?php
+  include_once('../includes/database.php');
   include_once('../includes/session.php');
   include_once('../database/database_user.php');
   
   $username=$_POST['username'];
   $password=$_POST['password'];
   
-  if($userID = verifyLogin($username, $password) != -1) {
+  if(($userID = verifyLogin($username, $password)) != -1) {
     setCurrentUser($userID, $username);
     header("Location:../php/homepage.php");}
 
