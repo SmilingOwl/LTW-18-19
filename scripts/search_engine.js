9206f2634192dd1d49c713abf2c9385d2792a96a
helenaMontenegro <36671963+helenaMontenegro@users.noreply.github.com>
2018-12-08 11:43:37 +0000

    Add files via upload

--- scripts/search_engine.js
+++ scripts/search_engine.js
@@ -16,6 +16,15 @@ let show_filtered_stories = function(event) {
         stories_to_delete[i].parentNode.removeChild(stories_to_delete[i]);
     let stories_ids = [];
     let flag_continue;
+    if(filtered_stories.length == 0) {
+        stories_to_add = document.createElement('section');
+        stories_to_add.id = "stories";
+        stories_to_add.innerHTML = "<h3>We couldn't find any stories!</h3>";
+        where_to_add.insertBefore(stories_to_add, footer_copyright);
+        return;
+    }
+    section_to_add = document.createElement('section');
+    section_to_add.id = "stories";
 
     for(let i = 0; i < filtered_stories.length; i++) {
         for(let j = 0; j < stories_ids.length; j++){
@@ -27,10 +36,9 @@ let show_filtered_stories = function(event) {
         if(flag_continue)
             continue;
         stories_ids.push(filtered_stories[i].story_id);
-        stories_to_add = document.createElement('section');
-        stories_to_add.id = "stories";
-        stories_to_add.innerHTML = '<article id="story">' +
-            '<header>' +
+        stories_to_add = document.createElement('article');
+        stories_to_add.id = "story";
+        stories_to_add.innerHTML = '<header>' +
                 '<h1><a href="story_item.php?story_id=' + filtered_stories[i].story_id+ '">' + filtered_stories[i].title + '</a></h1>' +
             '</header>' +
             '<p>' + filtered_stories[i].text + '</p>' +
@@ -39,23 +47,17 @@ let show_filtered_stories = function(event) {
                 '<span class="likes">' + filtered_stories[i].likes + ' <img src="../icons/like_icon.png" alt="likes"></span>' +
                 '<span class="dislikes">' + filtered_stories[i].dislikes + ' <img src="../icons/dislike_icon.png" alt="dislikes"></span>' +
                 '<span class="tasteChoice">' +
-                    '<a href="taste_choice_stories.php?id_taste=' + filtered_stories[i].id_taste + '">#' + filtered_stories[i].taste + '</a>' +
+                    '<a href="taste_choice_stories.php?id_taste=' + filtered_stories[i].id_taste + '">#taste</a>' +
                 '</span>' +
                 '<span class="comments">' + filtered_stories[i].comments +
                     '<img src="../icons/comment_icon.png" alt="comments"></span>' +
                 '<span class="favorites">'+ filtered_stories[i].favorites +' <img src="../icons/saved_icon.png" alt="favorites"></span>' +
                 '<input type="hidden" name="story_id" value="' + filtered_stories[i].story_id + '">' +
                 '<input type="hidden" name="user_id" value="' + filtered_stories[i].user_id + '">' +
-            '</footer>' +
-        '</article>';
-        where_to_add.insertBefore(stories_to_add, footer_copyright);
+            '</footer>';
+            section_to_add.appendChild(stories_to_add);
     }
-    if(filtered_stories.length == 0) {
-        stories_to_add = document.createElement('section');
-        stories_to_add.id = "stories";
-        stories_to_add.innerHTML = "<h3>We couldn't find any stories!</h3>";
-        where_to_add.insertBefore(stories_to_add, footer_copyright);
-    }
+    where_to_add.insertBefore(section_to_add, footer_copyright);
 }
 
 search_box.addEventListener('keypress', function(event){
