e6a8852652f075a44466ca8b15855ade1a7205e2
helenaMontenegro <36671963+helenaMontenegro@users.noreply.github.com>
2018-12-15 19:07:05 +0000

    Add files via upload

--- includes/session.php
+++ includes/session.php
@@ -5,4 +5,12 @@
       $_SESSION['username'] = $username;
       $_SESSION['user_id'] = $userID;
    }
+
+   function generate_random_token() {
+      return bin2hex(openssl_random_pseudo_bytes(32));
+     }
+   
+   if (!isset($_SESSION['csrf'])) {
+      $_SESSION['csrf'] = generate_random_token();
+   }
 ?>
