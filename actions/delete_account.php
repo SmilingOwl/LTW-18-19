1e9072b537c39c2c7b4eb5f6ae5b7788762df1b4
helenaMontenegro <36671963+helenaMontenegro@users.noreply.github.com>
2018-12-15 19:07:43 +0000

    Add files via upload

--- actions/delete_account.php
+++ actions/delete_account.php
@@ -3,12 +3,18 @@
   include_once('../database/database_user.php');
   include_once('../database/connection.php');
   include_once('../database/access_database.php');
+
+  // Verifies CSRF token
+  if ($_SESSION['csrf'] !== $_POST['csrf']) {
+    $_SESSION['messages'][] = array('type' => 'error', 'content' => 'Invalid Request!' );
+    header('Location: ../php/homepage.php');
+  }
   
   $user_id=$_POST['user_id'];
-  deleteUser($user_id);
+  $answer = deleteUser($user_id);
 
   session_destroy();
-  $users = get_all_users();
+  get_all_users();
 
-  echo json_encode($users);
+  echo json_encode($answer);
 ?>
---
+++ actions/get_csrf.php
@@ -0,0 +1,4 @@
+<?php
+    include_once('../includes/session.php');
+    echo json_encode($_SESSION['csrf']);
+?>
